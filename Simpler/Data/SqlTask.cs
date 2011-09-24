6b15107bef52cad28796e7fbc4b14ee1b1420d54
Greg Scott <i@gregoryjscott.com>
2011-09-24 13:48:55 -0800

    idea has changed to modifying fetchlist to expect sql in inputs

--- Simpler/Data/SqlTask.cs
+++ Simpler/Data/SqlTask.cs
@@ -1,80 +1,100 @@
-﻿using System.Data;
-using Simpler.Data.Tasks;
+﻿using Simpler.Data.Tasks;
 
 namespace Simpler.Data
 {
     // todo - just ideas
 
-    abstract class SqlTask : Task
-    {
-        // Inputs
-        public virtual IDbConnection OpenConnection { get; set; }
-    }
+//    abstract class SqlTask : Task
+//    {
+//        // Inputs
+//        public virtual IDbConnection OpenConnection { get; set; }
+//    }
 
-    abstract class FetchListUsingSqlTask : SqlTask
-    {
-        // Sub-tasks
-        public BuildParameters BuildParameters { get; set; }
-        public FetchList FetchList { get; set; }
+//    abstract class FetchListUsingSqlTask : SqlTask
+//    {
+//        // Sub-tasks
+//        public BuildParameters BuildParameters { get; set; }
+//        public FetchList FetchList { get; set; }
 
-        public override void Execute()
+//        public override void Execute()
+//        {
+//            using (var command = OpenConnection.CreateCommand())
+//            {
+//                command.Connection = OpenConnection;
+
+//                dynamic sqlTask = this;
+//                command.CommandText = sqlTask.Sql;
+
+//                // todo - look for a ConnectionName as well?
+
+//                if (Inputs != null)
+//                {
+//                    BuildParameters.CommandWithParameters = command;
+//                    BuildParameters.ObjectWithValues = Inputs;
+//                    BuildParameters.Execute();
+//                }
+
+//                // Fetch the list of SomeStuff and set the output property.
+//                FetchList.SelectCommand = command;
+//                FetchList.Execute();
+//                Outputs = new {FetchList.ObjectsFetched};
+//            }
+//        }
+//    }
+
+//    class FetchSomeStuff : FetchListUsingSqlTask
+//    {
+//        public string Sql =
+//            @"
+//            SELECT 
+//                SomeStuff
+//            FROM 
+//                ABunchOfJoinedTables
+//            WHERE 
+//                OneOfTheTables.SomeColumn = @Parameters.SomeCriteria 
+//            ";      
+//    }
+
+    //    class FetchSomeStuffUsingStatics : FetchListUsingSqlTask
+    //    {
+    //        public string Sql =
+    //            @"
+    //            SELECT 
+    //                SomeStuff
+    //            FROM 
+    //                ABunchOfJoinedTables
+    //            WHERE 
+    //                OneOfTheTables.SomeColumn = @Parameters.SomeCriteria 
+    //            ";
+
+    //        // Outputs
+    //        public SomeStuff SomeStuff { get; set; }
+
+    //        public override void Execute()
+    //        {
+    //            base.Execute();
+    //            SomeStuff = Outputs.ObjectsFetched;
+    //        }
+    //    }
+    class Program
+    {
+        public Program()
         {
-            using (var command = OpenConnection.CreateCommand())
+            var fetchList = TaskFactory<FetchList>.Create();
+            var list = fetchList.Execute(inputs: new
             {
-                command.Connection = OpenConnection;
-
-                dynamic sqlTask = this;
-                command.CommandText = sqlTask.Sql;
-
-                // todo - look for a ConnectionName as well?
-
-                if (Inputs != null)
-                {
-                    BuildParameters.CommandWithParameters = command;
-                    BuildParameters.ObjectWithValues = Inputs;
-                    BuildParameters.Execute();
-                }
-
-                // Fetch the list of SomeStuff and set the output property.
-                FetchList.SelectCommand = command;
-                FetchList.Execute();
-                Outputs = new {FetchList.ObjectsFetched};
-            }
+                ConnectionName = "MainDatabase",
+                Sql =
+                    @"
+                    SELECT 
+                        SomeStuff
+                    FROM 
+                        ABunchOfJoinedTables
+                    WHERE 
+                        OneOfTheTables.SomeColumn = @Parameters.SomeCriteria 
+                    "
+            }).Outputs;
         }
     }
 
-    class FetchSomeStuff : FetchListUsingSqlTask
-    {
-        public string Sql =
-            @"
-            SELECT 
-                SomeStuff
-            FROM 
-                ABunchOfJoinedTables
-            WHERE 
-                OneOfTheTables.SomeColumn = @Parameters.SomeCriteria 
-            ";      
-    }
-
-    class FetchSomeStuffUsingStatics : FetchListUsingSqlTask
-    {
-        public string Sql =
-            @"
-            SELECT 
-                SomeStuff
-            FROM 
-                ABunchOfJoinedTables
-            WHERE 
-                OneOfTheTables.SomeColumn = @Parameters.SomeCriteria 
-            ";
-
-        // Outputs
-        public SomeStuff SomeStuff { get; set; }
-
-        public override void Execute()
-        {
-            base.Execute();
-            SomeStuff = Outputs.ObjectsFetched;
-        }
-    }
 }
